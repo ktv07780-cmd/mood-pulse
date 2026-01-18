ab1b5a297ca003be5adf2c3ace5e714c1d2fe930
ktv07780-cmd <ktv07780@gmail.com>
2026-01-18 19:09:43 +0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,17 +1,17 @@
 /* === UI === */
 
 /* выбор режима */
-function setMode(m, el){
-    state.mode = m;
+window.setMode = function(m, el){
+    window.state.mode = m;
     document.querySelectorAll('.top button').forEach(b => b.classList.remove('selected'));
     el.classList.add('selected');
     document.getElementById('action-panel').style.display = 'flex';
 }
 
 /* переключение континентов */
-function toggleContinents(enabled){
+window.toggleContinents = function(enabled){
     if(enabled){
-        world.labelsData(continents)
+        window.world.labelsData(window.continents)
              .labelLat(d => d.lat)
              .labelLng(d => d.lng)
              .labelText(d => d.name)
@@ -20,11 +20,11 @@ function toggleContinents(enabled){
              .labelColor(() => 'rgba(255,255,255,0.9)')
              .labelResolution(2);
     } else {
-        world.labelsData([]);
+        window.world.labelsData([]);
     }
 }
 
 /* переключение сетки */
-function toggleGrid(enabled){
-    world.graticulesData(enabled ? [{}] : []);
+window.toggleGrid = function(enabled){
+    window.world.graticulesData(enabled ? [{}] : []);
 }
